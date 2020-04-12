5ba2b2a0f48ed861ece074419e80967b51f9cf87
Zeliang Yao <yaozeliangtest@outlook.com>
2020-04-12 12:40:57 +0200

    Add Update function

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import article_list,article_detail,article_create,article_safe_delete
-
-
+from .views import article_list,article_detail,article_create,article_update,article_safe_delete
 
 
 app_name = 'article'
@@ -10,6 +8,7 @@ urlpatterns = [
     path('article-list',article_list,name="article_list"),
     path('article-detail/<int:id>/', article_detail, name='article_detail'),
     path('article-create/', article_create, name='article_create'),
+    path('article-update/<int:id>/', article_update, name='article_update'),
     path('article-safe-delete/<int:id>/',article_safe_delete,name='article_safe_delete'),
 ]
 
--- article/views.py
+++ article/views.py
@@ -11,7 +11,7 @@ from .forms import ArticlePostForm
 # 引入User模型
 from django.contrib.auth.models import User
 
-
+@csrf_protect
 def article_list(request):
 
     articles = ArticlePost.objects.all()
@@ -19,6 +19,8 @@ def article_list(request):
 
     return render(request, 'article/list.html', context)
 
+
+@csrf_protect
 def article_detail(request, id):
     article = ArticlePost.objects.get(id=id)
 
@@ -35,7 +37,7 @@ def article_detail(request, id):
     context = { 'article': article }
     return render(request, 'article/detail.html', context)
 
-
+@csrf_protect
 def article_create(request):
     # 判断用户是否提交数据
     if request.method == "POST":
@@ -65,7 +67,36 @@ def article_create(request):
         # 返回模板
         return render(request, 'article/create.html', context)
 
-# 删文章
+@csrf_protect
+def article_update(request, id):
+
+    article = ArticlePost.objects.get(id=id)
+
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            # 完成后返回到修改后的文章中。需传入文章的 id 值
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+
+    # 如果用户 GET 请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文，将 article 文章对象也传递进去，以便提取旧的内容
+        context = { 'article': article, 'article_post_form': article_post_form }
+        # 将响应返回到模板中
+        return render(request, 'article/update.html', context)
+
+
+
+
+
 @csrf_protect
 def article_safe_delete(request, id):
     if request.method == 'POST':
@@ -74,3 +105,6 @@ def article_safe_delete(request, id):
         return redirect("article:article_list")
     else:
         return HttpResponse("仅允许post请求")
+
+
+
