2875e73dfd0b00929c5998c95d8f917beb2a68cd
Zeliang Yao <yaozeliangtest@outlook.com>
2020-04-16 23:29:02 +0200

    Heroku config files and updates

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'simpleui',
+    # 'simpleui',
     'crispy_forms', 
     'password_reset',
     'article.apps.ArticleConfig',
