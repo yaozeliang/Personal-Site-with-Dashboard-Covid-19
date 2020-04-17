bbf1dc189b8567d5bdff0b528f6b3523267d4699
Zeliang Yao <yaozeliangtest@outlook.com>
2020-04-17 21:26:59 +0200

    Make it better

--- article/urls.py
+++ article/urls.py
@@ -5,7 +5,7 @@ from .views import article_list,article_detail,article_create,article_update,art
 app_name = 'article'
 
 urlpatterns = [
-    path('article-list',article_list,name="article_list"),
+    path('article-list/',article_list,name="article_list"),
     path('article-detail/<int:id>/', article_detail, name='article_detail'),
     path('article-create/', article_create, name='article_create'),
     path('article-update/<int:id>/', article_update, name='article_update'),
